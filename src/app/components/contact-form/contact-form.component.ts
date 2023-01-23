8a51f0d272c062e2cd8b52d6cb69af786fb3db97
clintonjosephs <clintonjosephs26@gmail.com>
2023-01-23 23:06:16 +0000

    Wrap up for fgit

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  comment: any;
+  emailaddrs: any;
+  phonenum: any;
+  lname: any;
+  fname: any;
+  email_sent = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  submitForm(form: NgForm) {
+    this.email_sent = false;
+    const formData = { name: this.fname + '' + this.lname, email: this.emailaddrs, phone: this.phonenum, message: this.comment };
+    this.http.post('https://formspree.io/f/mbjelebp', formData).subscribe(
+      (response) => {
+        this.email_sent = true;
+        console.log(response);
+        form.resetForm();
+      },
+      (error) => console.log(error)
+    );
+  }
 }
